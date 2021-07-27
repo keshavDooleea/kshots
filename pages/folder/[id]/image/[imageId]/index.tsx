26ef192d56412b47a891c7193b6894e3a558fa0a
keshav <kdooleea@yahoo.ca>
2021-07-27 13:54:58 -0400

    fetching folder on /folder/id/image/id

--- pages/folder/[id]/image/[imageId]/index.tsx
+++ pages/folder/[id]/image/[imageId]/index.tsx
@@ -4,18 +4,24 @@ import CommonLayout from "../../../../../Components/CommonLayout";
 import Error from "../../../../../Components/Error";
 import { getDate, getDecodedBase64, isOnlyNumber } from "../../../../../utils/lib/config";
 import { GET } from "../../../../../utils/lib/http";
-import { IDBImage } from "../../../../../utils/lib/intefaces";
+import { IDBFolder, IDBImage } from "../../../../../utils/lib/intefaces";
 import styles from "../../../../../styles/Image.module.scss";
+import { faFolder, faImage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface ImageItemProp {
   itemName: string;
   property: string;
+  icon?: IconProp;
+  color?: string;
 }
 
 const ImageId = () => {
   const router = useRouter();
   const [error, setError] = useState<string>("");
   const [image, setImage] = useState<IDBImage>();
+  const [folder, setFolder] = useState<IDBFolder>();
 
   useEffect(() => {
     if (!router.isReady) return;
@@ -42,31 +48,44 @@ const ImageId = () => {
       }
     };
 
+    const fetchColor = async () => {
+      const { code, message, data } = await GET<IDBFolder>(`folder/${id}`);
+
+      if (code === 200) {
+        setFolder(data);
+        return;
+      }
+
+      setError(message);
+    };
+
+    fetchColor();
     fetchImages();
   }, [router.isReady]);
 
-  const ImageItem = ({ itemName, property }: ImageItemProp) => {
+  const ImageItem = ({ itemName, property, icon, color }: ImageItemProp) => {
     return property ? (
       <div className={styles.item}>
-        <h4>{itemName}</h4>
+        <h4>
+          {icon && <FontAwesomeIcon icon={icon} className={styles.icon} style={{ color: color ? color : "auto" }} />}
+          {itemName}
+        </h4>
         <p className="subtitle">{property}</p>
       </div>
     ) : (
       <></>
     );
-
-    return <></>;
   };
 
   return (
-    <CommonLayout title={"Image"} returnUrl={`/folder/${router.query.id}`}>
+    <CommonLayout title={"Image"} returnUrl={`/folder/${router.query.id}`} customBgColor={folder?.color}>
       {!error && (
         <>
           <div className={styles.main}>
             {(image?.title || image?.description) && (
               <div className={styles.left}>
-                <ImageItem itemName="Title" property={image?.title as string} />
-                <ImageItem itemName="Created On" property={getDate(image?.createdat as Date)} />
+                <ImageItem itemName="Title" property={image?.title as string} icon={faImage} color={folder?.color} />
+                <ImageItem itemName="Folder Name" property={folder?.name as string} icon={faFolder} color={folder?.color} />
                 <ImageItem itemName="Description" property={image?.description as string} />
               </div>
             )}
