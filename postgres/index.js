0d478695f979811b545d28105c72f4e2668d2d2f
keshav <kdooleea@yahoo.ca>
2021-07-28 17:22:10 -0400

    fixed syntax mistake in pool constructor

--- postgres/index.js
+++ postgres/index.js
@@ -3,9 +3,9 @@ import isLocal from "../utils/lib/config";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl = {
+  ssl: {
     rejectUnauthorized: false,
-  }
+  },
 });
 
 // if (!isLocal) {
