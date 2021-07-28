d2abf6ff0989576f7490021fd12b6f90dd7b6ca3
keshav <kdooleea@yahoo.ca>
2021-07-28 12:48:58 -0400

    remodified db name

--- utils/lib/config.ts
+++ utils/lib/config.ts
@@ -8,9 +8,9 @@ export const COLORS = ["rosybrown", "#fbae3c", "#a5a58d", "#669bbc", "#177e89", 
 
 // POSTGRESS
 const DB_NAME = isLocal ? "kshots" : process.env.HEROKU_DB_NAME;
-export const USERS_SCHEMA = `${process.env.HEROKU_DB_NAME}.Users`;
-export const FOLDERS_SCHEMA = `${process.env.HEROKU_DB_NAME}.Folders`;
-export const IMAGES_SCHEMA = `${process.env.HEROKU_DB_NAME}.Images`;
+export const USERS_SCHEMA = "kshots.Users";
+export const FOLDERS_SCHEMA = "kshots.Folders";
+export const IMAGES_SCHEMA = "kshots.Images";
 export const IMG_SRC = `convert_from(decode(encode(src, 'base64'), 'base64'), 'UTF8')`;
 export const DECODE_IMG = (src: string) => `decode(${src.split(",")[1]}, 'base64')`; // base64 encoding
 
